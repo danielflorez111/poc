70d0a5a9e2958e1d1ea90b25a6b90567f85db6ec
Daniel Florez <danielflorez111@gmail.com>
2022-05-17 16:40:59 +0200

    Test Env variables

--- poc/settings.py
+++ poc/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'poc.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('db'),
+        default=os.getenv('DB_URL'),
         conn_max_age=600
     )
 }
