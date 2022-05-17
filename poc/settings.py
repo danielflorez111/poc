03b01b388b21e57cd486e9812c7558b627dff6aa
Daniel Florez <danielflorez111@gmail.com>
2022-05-17 13:10:51 +0200

    Test

--- poc/settings.py
+++ poc/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z%7^9@hec*pmvgkn_n7x2*%@j%wp@3nh)wc93r_6xn711vg=v!'
 DEBUG = False
 ALLOWED_HOSTS = ['poc-b0cf.onrender.com']
+CSRF_TRUSTED_ORIGINS=['poc-b0cf.onrender.com']
 
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = int(os.environ.get("DEBUG", default=0))
