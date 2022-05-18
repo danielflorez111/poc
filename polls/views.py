2cd91e291309ec9a84674386377ac4f667e6dc12
Daniel Florez <danielflorez111@gmail.com>
2022-05-18 12:26:23 +0200

    Test Django-Q

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.template import RequestContext
+from django_q.tasks import async_task, result
 
 
 def index(request):
-    return HttpResponse("Pipeline POC using Render")
+    template = loader.get_template('index.html')
+    async_task('math.copysign', 2, -2)
+
+    return HttpResponse(template.render())
+
+def task(request):
+    context_instance = RequestContext(request)
+    return HttpResponse("Task")
