10c511d94c1ff07361375df1a5d5d64657fe03be
Daniel Florez <danielflorez111@gmail.com>
2022-05-18 10:29:36 +0200

    Test

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#echo "Collect static files"
+# python manage.py collectstatic --noinput
+
+# Migrate
+#python manage.py migrate || exit
+
+python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
