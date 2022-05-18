cd55f7bd5165e42ddeba3758ac666d80ae27bedb
Daniel Florez <danielflorez111@gmail.com>
2022-05-18 11:32:55 +0200

    Test redis

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Pipeline POC using Render")
+    return HttpResponse("Pipeline POC using Render - Test Redis preview env")
